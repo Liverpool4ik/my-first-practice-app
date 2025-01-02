c09c305fceab663d58b30f5c49f1238fa829756f
Liverpool4ik <liverpool4ik@proton.me>
2025-01-02 12:13:38 +0200

    <h2>newBranch commit 2</h2>

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -4,9 +4,9 @@ export const User = () => {
 	return (
 		<div>
 			<h2>User</h2>
-			<h2>newBranch commit 1</h2>
-			<h2>User</h2>
-			<h2>User</h2>
+			<h2>newBranch commit 2</h2>
+			<h2>newBranch commit 2</h2>
+			<h2>newBranch commit 2</h2>
 		</div>
 	);
 };
